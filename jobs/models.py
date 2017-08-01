789c1fe8b83cc7956244a1680420a95950b8a744
AbuzzT <mohsin.yamani@gmail.com>
2017-08-01 01:26:21 -0400

    images

--- jobs/models.py
+++ jobs/models.py
@@ -18,10 +18,11 @@ CATEGORIES = (
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     help_text = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to="categories/", blank=True, null=True)
+    image_url = models.CharField(max_length=200)
+    # image = models.ImageField(upload_to="categories/", blank=True, null=True)
 
     def __str__(self):
         return self.name
